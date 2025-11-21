80f7e91a77d9b0a528076b1f2104f63b3a2febcb
RealCharlesWallace <realCharlesWallace@gmail.com>
2025-11-21 10:26:52 -0800

    Polyfill ArrayBuffer resizable flags for Vitest jsdom

--- tests/setup.ts
+++ tests/setup.ts
@@ -9,3 +9,29 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+// Polyfill missing ArrayBuffer/SharedArrayBuffer resizable/growable flags in older Node runtimes
+const abDescriptor = Object.getOwnPropertyDescriptor(ArrayBuffer.prototype, 'resizable');
+if (!abDescriptor) {
+  Object.defineProperty(ArrayBuffer.prototype, 'resizable', {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return false;
+    },
+  });
+}
+
+// Some environments lack SharedArrayBuffer
+if (typeof SharedArrayBuffer !== 'undefined') {
+  const sabDescriptor = Object.getOwnPropertyDescriptor(SharedArrayBuffer.prototype, 'growable');
+  if (!sabDescriptor) {
+    Object.defineProperty(SharedArrayBuffer.prototype, 'growable', {
+      configurable: true,
+      enumerable: false,
+      get() {
+        return false;
+      },
+    });
+  }
+}
